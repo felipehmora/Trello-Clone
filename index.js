de280fed5c52e60947e9210ab19d3194e024cd2c
felipehmora <felipehmoraarg@gmail.com>
2024-11-24 15:51:07 -0400

    detalles

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const input = document.getElementById("input");
 const addBtn = document.getElementById("add-btn");
+const addTask = document.getElementById("add-task");
+const task = document.createElement("span");
 
-addBtn.addEventListener("click", () => {
+addTask.addEventListener("click", () => {
   console.log(input.value);
 });
