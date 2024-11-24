664c5567fe89eeb28d9f765076195143b9456319
felipehmora <felipehmoraarg@gmail.com>
2024-11-24 18:29:22 -0400

    agregando más lógica

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const mainContainer = document.querySelector(".main-container");
-const input = document.getElementById("task-input");
+const createCardInput = document.getElementById("card-input");
 const cardInput = document.getElementById("card-input");
 const addBtn = document.getElementById("add-btn");
 const addTask = document.getElementById("add-task");
 const newBoard = document.createElement("div");
 
 addBtn.addEventListener("click", () => {
+  let title = createCardInput.value;
+
   newBoard.className = "card";
 
   newBoard.innerHTML = `
-        <h2>Untitled card 1</h2>
+        <h2>${title}</h2>
         <input type="text" placeholder="Add Task" id="task-input" />
         <button id="add-task">+</button>
         `;
 
   mainContainer.appendChild(newBoard);
+  console.log(createCardInput.value);
 });
