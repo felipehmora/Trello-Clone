057c12b1ada2e08e479cc5c19d1e5aec605938e9
felipehmora <felipehmoraarg@gmail.com>
2024-11-24 18:16:07 -0400

    logica basica de agregar tarjetas

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const input = document.getElementById("input");
+const mainContainer = document.querySelector(".main-container");
+const input = document.getElementById("task-input");
+const cardInput = document.getElementById("card-input");
 const addBtn = document.getElementById("add-btn");
 const addTask = document.getElementById("add-task");
-const task = document.createElement("span");
+const newBoard = document.createElement("div");
 
-addTask.addEventListener("click", () => {
-  console.log(input.value);
+addBtn.addEventListener("click", () => {
+  newBoard.className = "card";
+
+  newBoard.innerHTML = `
+        <h2>Untitled card 1</h2>
+        <input type="text" placeholder="Add Task" id="task-input" />
+        <button id="add-task">+</button>
+        `;
+
+  mainContainer.appendChild(newBoard);
 });
